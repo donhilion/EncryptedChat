6851ea7d364b7d752725d16cfb57d2be83c8260d
donhilion <donhilion@googlemail.com>
2012-12-03 17:29:49 +0100

    Startet to change to use Message.

--- Client/communication/server_connection.py
+++ Client/communication/server_connection.py
@@ -3,6 +3,7 @@
 '''
 
 from tcp_connection import TCPConnection
+from message import Message
 import logging
 
 MSG_GET_SERVER_KEY = 'plain:getServerKey\n'
@@ -17,24 +18,32 @@ class ServerConnection(object):
 		_id_to_handler: The dictionary of handlers for respondes with a special id.
 	'''
 	def __init__(self):
+		''' The constructor.
+		'''
 		self._tcp_connection = TCPConnection()
 		self._next_id = 0
 		self._id_to_respond = {}
 		self._id_to_handler = {}
 
 	def get_server_key(self):
-		# TODO check connection
-		send_msg = str(self._next_id) + ':' + MSG_GET_SERVER_KEY
+		''' Gets the server rsa key.
+		'''
+		if self._tcp_connection is None or \
+			not self._tcp_connection.is_connected:
+			return None
+		send_msg = Message(id=self._next_id, encoded=False, \
+			msg=MSG_GET_SERVER_KEY)
 		self._next_id += 1
 
-		self._tcp_connection.send(send_msg)
+		self._tcp_connection.send(send_msg.get_json_string())
+		return None
 
 	def poll_for_msgs(self):
 		msgs = self._tcp_connection.receive()
 		for msg in msgs:
 			try:
 				divivder = msg.index(':')
-				id = msg[:divivder]
-				self._id_to_respond[id] = msg[divivder+1:]
+				msg_id = msg[:divivder]
+				self._id_to_respond[msg_id] = msg[divivder+1:]
 			except Exception:
 				logging.error('Returned message in unknown format: ' + str(msg))
